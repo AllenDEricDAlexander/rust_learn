36083a0a7cfa2daf65e9138de94740587fd93f23
luofu <mqa010225@163.com>
2023-12-13 22:35:18 +0800

    <feat>(<hecto>): <chapter-2>
    reading user input
      - stdin && keymap
      - error operation

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -5,20 +5,39 @@ fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for byte in io::stdin().bytes() {
-        let c = byte.unwrap() as char;
-        println!("{}", c);
-
         let b = byte.unwrap();
         let c = b as char;
-        if c.ischar() {
+        if c.is_control() {
             println!("{:?}\r", b);
         } else {
             println!("{:?}({:?})\r", b, c);
         }
-
-
-        if c == 'q' {
+        if b == to_ctrl_byte('q') {
             break;
         }
+
+        match b {
+            OK(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?}\r", b);
+                } else {
+                    println!("{:?}({:?})\r", b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
+        }
     }
+}
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error) {
+    println!("{}", e);
 }
