227b72415ab241d8df63dc009a8445c435ef0566
luofu <mqa010225@163.com>
2023-12-13 17:55:20 +0800

    <feat>(<hecto>): <chapter-1>
    setup
      - stdin && stdout
      - quit operation

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{self, Read, stdout};
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for byte in io::stdin().bytes() {
+        let c = byte.unwrap() as char;
+        println!("{}", c);
+
+        let b = byte.unwrap();
+        let c = b as char;
+        if c.ischar() {
+            println!("{:?}\r", b);
+        } else {
+            println!("{:?}({:?})\r", b, c);
+        }
+
+
+        if c == 'q' {
+            break;
+        }
+    }
 }
