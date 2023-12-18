bab26cda982aae870ae1e776f578b0034382ef28
luofu <mqa010225@163.com>
2023-12-18 14:00:05 +0800

    <feat>(<hecto>): <chapter-3>
    display screen
      - move cursor
      - display welcome message

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,15 +1,22 @@
-use std::io::{self, stdout, Write};
-use termion::raw::IntoRawMode;
+use crate::Terminal;
 use termion::event::Key;
-use termion::input::TermRead;
+use std::io;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+    x: usize,
+    y: usize,
+}
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(error);
@@ -24,46 +31,90 @@ impl Editor {
     }
 
     pub fn default() -> Editor {
-        Self { should_quit: false }
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cursor_position: Position { x: 0, y: 0 },
+        }
     }
 
     fn refresh_screen(&self) -> Result<(), io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        Terminal::cursor_hide();
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Goodbye!\r");
         } else {
             self.draw_rows();
-            println!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_position(&self.cursor_position);
         }
-        stdout().flush()?;
+        Terminal::cursor_show();
+        Terminal::flush().expect("TODO: panic message");
         Ok(())
     }
 
     fn process_keypress(&mut self) -> Result<(), io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
     }
 
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut y, mut x } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            Key::Home => x = 0,
+            Key::End => x = width,
+            _ => (),
+        }
+        self.cursor_position = Position { x, y }
+    }
+
     fn draw_rows(&self) {
-        for _ in 0..24 {
-            print!("!\r");
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
     }
-}
 
-fn read_key() -> Result<Key, io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
     }
 }
 
 fn die(e: io::Error) {
-    println!("{}", termion::clear::All);
+    Terminal::clear_screen();
     println!("{}", e);
 }
